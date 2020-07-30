052cca62189ecd47e72cec0ab24c16fcf6f206f2
hezibu <hezibuba@mail.tau.ac.il>
2020-07-30 19:13:11 +0300

    - Added PPMR quadtratic model - Removed size term from alien interaction

--- get_models_a.R
+++ get_models_a.R
@@ -1,28 +1,39 @@
 source("functions.R")
 
-full.model.a.1 <- lmer(log_a ~ log_arena + water +  temp*log_pred*log_prey*aspect_ratio + 
-                         alien * temp * log_pred + (1|source) + (1|prey_type_coarse),
-                       fd,REML = F )
+
+full.model.a.1 <- lmer(log_a ~ log_arena  + water +  temp*log_pred*log_prey*aspect_ratio + 
+                         alien  * temp  + (1|source) + (1|prey_type_coarse),
+                       data_for_analysis,REML = F )
 dredged.a.1 <- par_dredge(model = full.model.a.1,
-                          data = fd,
+                          data = data_for_analysis,
                           cores = 7)
 best.model.a.1 <- get.models(dredged.a.1,subset = delta==0)[[1]]
 
 full.model.a.2 <- lmer(log_a ~ log_arena + water + poly(temp,2)*log_pred*log_prey*aspect_ratio + 
-                         alien * poly(temp,2) * log_pred +
-                         (1|source) + (1|prey_type_coarse),fd,REML = F )
+                         alien * poly(temp,2)  +
+                         (1|source) + (1|prey_type_coarse),data_for_analysis,REML = F )
 dredged.a.2 <- par_dredge(model = full.model.a.2,
-                          data = fd,
+                          data = data_for_analysis,
                           cores = 7)
 best.model.a.2 <- get.models(dredged.a.2,subset = delta==0)[[1]]
 
 
 
 full.model.a.ppmr <- lmer(log_a ~ log_arena + water + temp * ppmr * aspect_ratio + 
-                            alien * ppmr * temp + 
+                            alien * temp + 
                             (1|source) + (1|prey_type_coarse),
-                          fd.ppmr, REML = F)
+                          data_for_analysis, REML = F)
 ppmr.dredge <- par_dredge(model = full.model.a.ppmr,
-                          data = fd.ppmr,
+                          data = data_for_analysis,
                           cores = 7)
 best.model.a.ppmr <- get.models(ppmr.dredge,delta == 0)[[1]]
+
+
+full.model.a.ppmr.2 <- lmer(log_a ~ log_arena + water + temp * poly(ppmr,2) * aspect_ratio + 
+                            alien * temp + 
+                            (1|source) + (1|prey_type_coarse),
+                            data_for_analysis, REML = F)
+ppmr.dredge.2 <- par_dredge(model = full.model.a.ppmr.2,
+                          data = data_for_analysis,
+                          cores = 7)
+best.model.a.ppmr.2 <- get.models(ppmr.dredge.2,delta == 0)[[1]]
--- get_models_h.R
+++ get_models_h.R
@@ -1,28 +1,39 @@
 source("functions.R")
 
+data_for_analysis <- left_join(fd,fd.ppmr)
+
 full.model.h.1 <- lmer(log_h ~ log_arena + water + temp*log_pred*log_prey*aspect_ratio + 
-                         alien * temp * log_pred + (1|source) + (1|prey_type_coarse),
-                       fd,REML = F )
+                         alien * temp + (1|source) + (1|prey_type_coarse),
+                       data_for_analysis,REML = F )
 dredged.h.1 <- par_dredge(model = full.model.h.1,
-                          data = fd,
+                          data = data_for_analysis,
                           cores = 7)
 best.model.h.1 <- get.models(dredged.h.1,subset = delta==0)[[1]]
 
 full.model.h.2 <- lmer(log_h ~ log_arena + water + poly(temp,2)*log_pred*log_prey*aspect_ratio + 
-                         alien * poly(temp,2) * log_pred +
-                         (1|source) + (1|prey_type_coarse),fd,REML = F )
+                         alien * poly(temp,2) +
+                         (1|source) + (1|prey_type_coarse),data_for_analysis,REML = F )
 dredged.h.2 <- par_dredge(model = full.model.h.2,
-                          data = fd,
+                          data = data_for_analysis,
                           cores = 7)
 best.model.h.2 <- get.models(dredged.h.2,subset = delta==0)[[1]]
 
 
 
 full.model.h.ppmr <- lmer(log_h ~ log_arena + water + temp * ppmr * aspect_ratio + 
-                            alien * ppmr * temp + 
+                            alien * temp + 
                             (1|source) + (1|prey_type_coarse),
-                          fd.ppmr, REML = F)
+                          data_for_analysis, REML = F)
 ppmr.dredge <- par_dredge(model = full.model.h.ppmr,
-                          data = fd.ppmr,
+                          data = data_for_analysis,
                           cores = 7)
 best.model.h.ppmr <- get.models(ppmr.dredge,delta == 0)[[1]]
+
+full.model.h.ppmr.2 <- lmer(log_h ~ log_arena + water + temp * poly(ppmr,2) * aspect_ratio + 
+                              alien * temp + 
+                              (1|source) + (1|prey_type_coarse),
+                            data_for_analysis, REML = F)
+ppmr.dredge.2 <- par_dredge(model = full.model.h.ppmr.2,
+                            data = data_for_analysis,
+                            cores = 7)
+best.model.h.ppmr.2 <- get.models(ppmr.dredge.2,delta == 0)[[1]]
